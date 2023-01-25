5667a00606c33035815517a2b06e276e0c4daff2
Diego Dias <diasdsdiego@gmail.com>
2023-01-25 08:01:16 -0300

    books

---
+++ database/migrations/2023_01_25_095791_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Enums\BookStatusEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('classification_id')->constrained('classification_books')->references('id');
+            $table->string('name', 50);
+            $table->string('author', 50);
+            $table->enum('status', BookStatusEnum::names());
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
