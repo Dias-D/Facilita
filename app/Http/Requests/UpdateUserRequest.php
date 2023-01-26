fe5fa405abe0447cca2d07050f81af14263a76e5
Diego Dias <diasdsdiego@gmail.com>
2023-01-26 08:24:29 -0300

    finish user update and adjust views

---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'  => 'unique:users,name,' . Auth::user()->id . ',id',
+            'email' => 'email:rfc,dns|unique:users,email,' . Auth::user()->id . ',id'
+        ];
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,26 +39,26 @@
         <div class="flex flex-row">
             <div class="mx-10">
                 <div class="flex flex-col overflow-hidden bg-white rounded-lg">
-                    <div class="flex flex-col px-12 py-10 bg-white">
+                    <div class="flex flex-col px-6 py-5 bg-white">
                         <div class="mt-1">
                             <form method="post" action="/login" novalidate>
                                 @csrf
                                 <label class="block">
-                                    <span class="text-lg font-medium text-gray-800">E-mail</span>
-                                    <input type="email" name="email" class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded" />
+                                    <span class="text-md font-medium text-gray-800">E-mail</span>
+                                    <input type="email" name="email" class="block w-full px-2 py-1 mt-1 text-md bg-gray-100 border-2 border-transparent rounded" />
                                     @error('email')
                                     <span class="block font-medium text-brand-danger">{{ $message }}</span>
                                     @enderror
                                 </label>
                                 <label class="block mt-6">
-                                    <span class="text-lg font-medium text-gray-800">Senha</span>
-                                    <input type="password" name="password" class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded" />
+                                    <span class="text-md font-medium text-gray-800">Senha</span>
+                                    <input type="password" name="password" class="block w-full px-2 py-1 mt-1 text-md bg-gray-100 border-2 border-transparent rounded" />
                                     @error('password')
                                     <span class="block font-medium text-brand-danger">{{ $message }}</span>
                                     @enderror
                                 </label>
                                 <div class="flex justify-center">
-                                    <button class="px-8 py-3 mt-10 font-bold text-white rounded bg-green-500 focus:outline-none">
+                                    <button class="px-4 py-1 mt-6 font-bold text-white rounded bg-green-500 focus:outline-none">
                                         <span>Entrar</span>
                                     </button>
                                 </div>
